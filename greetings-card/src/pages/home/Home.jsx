56e5b06364f137d99311736b7bea83b0a76ea8c8
Paresh Lamichhane <pareshlamichhane@gmail.com>
2023-10-19 10:33:08 +0545

    Staged changes

---
+++ greetings-card/src/pages/home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+    return (
+      
+    <div className="App">
+    <header className="App-header">
+      <body>
+
+      <p className='code'>
+        Create custom greetings card and save them and send them.
+      </p>
+      <a
+        className="App-link"
+        href="/create-card"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        Get started
+      </a>
+        </body>
+    </header>
+  </div>
+    )
+}
+
+
+export default Home;
